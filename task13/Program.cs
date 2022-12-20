93e8cd9b88590a906969d74be0fd4a80535bd60a
unknown <veronika040182@hotmail.com>
2022-12-20 19:32:35 +0300

    задача 13 переделана

--- task13/Program.cs
+++ task13/Program.cs
@@ -5,6 +5,21 @@
 78 -> третьей цифры нет
 32679 -> 6  */ 
 
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);    // ввод числа с консоли
+int digit = (int)Math.Log10(num)-2;          // метод с логорифм работает до 9-ти значных чисел, включительно               
+                                              
+ if (digit > 0)
+ {
+	Console.WriteLine($"третьей цифра числа {num} является {num % (int)Math.Pow(10, digit + 1) / (int)Math.Pow(10, digit) }" );
+ }
+else
+{
+	Console.WriteLine( "нет третьей цифры" );
+}
+
+
+/* // Нахождения числа при помощи строки
 
 Console.WriteLine("введите число");
 int num =Convert.ToInt32(Console.ReadLine());
@@ -16,3 +31,10 @@ else
     {
 		Console.WriteLine("третьего числа нет");
 	}
+{
+	Console.WriteLine($"третья цифра числа {num} является {digit}");
+}*/
+
+
+
+
